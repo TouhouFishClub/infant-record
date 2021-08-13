d2bc8f8565f847709112af74edda0a7d8d14aaca
flandre <flantan233@gmail.com>
2021-08-13 14:28:22 +0800

    change icp

--- front/src/views/UserForm.vue
+++ front/src/views/UserForm.vue
@@ -1,20 +1,25 @@
 <template>
-  <v-app class="main-container">
-    <v-card
-      class="mx-auto my-auto"
-      max-width="344"
-    >
-      <Logo
-        ref="logo"
-        :width="100"
-        :height="100"
-        :strokeWidth="20"
-        class="logo"
-      />
-      <v-card-text>
-        <router-view/>
-      </v-card-text>
-    </v-card>
+  <v-app>
+    <div class="main-container">
+      <v-card
+          class="mx-auto my-auto"
+          min-width="300"
+      >
+        <Logo
+            ref="logo"
+            :width="100"
+            :height="100"
+            :strokeWidth="20"
+            class="logo"
+        />
+        <v-card-text>
+          <router-view/>
+        </v-card-text>
+      </v-card>
+      <div class="icp">
+        <a href="https://beian.miit.gov.cn/">京ICP备20031747号-1</a>
+      </div>
+    </div>
   </v-app>
 </template>
 
@@ -38,6 +43,18 @@
     align-items: center;
     justify-content: center;
     background-color: #f36392 !important;
+    height: 100%;
+  }
+  .icp {
+    width: 100%;
+    position: fixed;
+    bottom: 20px;
+    text-align: center;
+    a {
+      text-decoration: none;
+      font-size: 14px;
+      color: #fff;
+    }
   }
   .logo {
     margin: 30px auto 0;
